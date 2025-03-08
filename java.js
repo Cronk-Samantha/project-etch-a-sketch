ebb042e67520f6c12d85c62e3bb93dc33eb00c43
Cronk-Samantha <195992994+Cronk-Samantha@users.noreply.github.com>
2025-03-07 18:32:33 -0700

    Adjusted my JS to prompt the user for input.
    Created a new function so when the user clicks the
    generate button a prompt window will appear where user
    can enter a number. If its valid, the new grid is
    generated and if its not a valid # the user is asked
    to try again,

--- java.js
+++ java.js
@@ -1,12 +1,26 @@
 const flexContainer = document.querySelector("#container");
 
-const gridSize = 16;
+document.getElementById("generate").onclick = userPrompt;
 
-// For loop to create 16 X 16 div elements
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    flexContainer.appendChild(div);
+function userPrompt() {
+    let userInput = prompt("Please enter the number of desired squares per side (Example: Entering the number 3 would result in 9 total squares in your grid because 3 * 3 = 9)");
+    if (userInput !== null && !isNaN(userInput) && userInput >= 1 && userInput <= 100) {
+        let gridSize = parseInt(userInput);
+        generateGrid(gridSize);
+    } else {
+        alert("Invalid input. Please try again.");
+        return userPrompt();
+    }
+}
+
+
+function generateGrid(gridSize) {
+    flexContainer.innerHTML = "";
+        for (let i = 0; i < gridSize * gridSize; i++) {
+            const div = document.createElement("div");
+            div.classList.add("grid-item");
+            flexContainer.appendChild(div);
+        }
 }
 
 const gridItems = document.querySelectorAll(".grid-item");
